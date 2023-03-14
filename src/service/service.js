7c1489e17298624f94fca6f5aadd11cbac8c18bb
Keifer Shearer <keifearer@gmail.com>
2023-03-14 10:36:00 +0100

    created service

--- src/service/service.js
+++ src/service/service.js
@@ -6,4 +6,35 @@ const myApi = axios.create({
     baseURL: BACKEND_URL,
 })
 
+myApi.allPost = (post) => {
+    return myApi.get('/posts/')
+}
+
+myApi.createPost = (post) => {
+    return myApi.post('/posts/create')
+}
+
+myApi.getSpecificPost = (postId) => {
+    return myApi.get(`posts/${postId}`)
+}
+
+myApi.updatePost = (postId, post) => {
+    return myApi.patch(`/posts/${postId}`, post)
+}
+
+myApi.deletePost = (postId) => {
+    return myApi.delete(`/posts/${postId}`)
+}
+
+myApi.availableTutor = (tutor) => {
+    return myApi.get('/available-tutors')
+}
+
+myApi.myProfile = (user) => {
+    return myApi.get('/profile')
+}
+
+myApi.otherProfile = (profileId) => {
+    return myApi.get(`/profile/${profileId}`)
+}
 export default myApi
