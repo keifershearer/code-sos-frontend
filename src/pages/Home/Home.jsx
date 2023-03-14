dc99f892fa3df4ee11937ece43ab3dbe75515eda
Keifer Shearer <keifearer@gmail.com>
2023-03-14 16:33:00 +0100

    created is Tutor

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../../context/auth.context'
 
 const Home = () => {
+  const {user} = useContext(AuthContext)
+  console.log(user)
   return (
     <div className='home-page'>
       <h1>CodeSOS</h1>
-      <p>Get help in a coding emergenncy</p>
+      <p>Get help in a coding emergency</p>
+      {!user ? (
       <div className='home-page-button'>
       <Link to='/auth/login'><button>Login</button></Link>
       <Link to='/auth/signup'><button>Signup</button></Link>
-      </div>
+      </div>) : (<p>Welcome {user.username}</p>)}
     </div>
   )
 }
