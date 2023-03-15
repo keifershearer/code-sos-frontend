89684a291acd394177341feeac0fff1ed55c1bcb
TomMahe <mahettom@gmail.com>
2023-03-15 16:29:21 +0100

    modify the details update/delete/details

--- src/pages/posts/EditPost/EditPost.jsx
+++ src/pages/posts/EditPost/EditPost.jsx
@@ -8,24 +8,24 @@ const EditPost = () => {
   const params = useParams()
   const navigate = useNavigate()
 
-  useEffect(() =>{
+  useEffect(() => {
     myApi
-    .getSpecificPost(params.postId)
-    .then((res) => {
-      setQuestion(res.data.question)
-      setCode_Example(res.data.code_example)
-    })
-    .catch((e) => console.error(e))
+      .getSpecificPost(params.postId)
+      .then((res) => {
+        setQuestion(res.data.question)
+        setCode_Example(res.data.code_example)
+      })
+      .catch((e) => console.error(e))
 
   }, [])
-  const handleSubmit = async (e) =>{
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    const postToUpdate = {question, code_example}
+    const postToUpdate = { question, code_example }
 
     try {
       const updatedPost = await myApi.updatePost(params.postId, postToUpdate)
-      if(updatedPost.status === 202){
-        navigate('/post')
+      if (updatedPost.status === 202) {
+        navigate('/posts')
       }
     } catch (error) {
       console.error(error)
@@ -36,24 +36,24 @@ const EditPost = () => {
       <label htmlFor='question'>Question: </label>
       <div>
         <textarea
-        value={question}
-        name="question"
-        id="question"
-        cols='30'
-        rows='10'
-        onChange={(e) => setQuestion(e.target.value)}
+          value={question}
+          name="question"
+          id="question"
+          cols='30'
+          rows='10'
+          onChange={(e) => setQuestion(e.target.value)}
         ></textarea>
       </div>
       <div>
         <label htmlFor="codeExample">Code Example: </label>
-        <textarea 
-        value={code_example}
-        name="coeExample"
-         id="codeExample" 
-         cols="30" rows="10"
+        <textarea
+          value={code_example}
+          name="coeExample"
+          id="codeExample"
+          cols="30" rows="10"
           onChange={(e) => setCode_Example(e.target.value)}>
-          </textarea>
-          <button>Update Post</button>
+        </textarea>
+        <button>Update Post</button>
       </div>
     </form>
   )
